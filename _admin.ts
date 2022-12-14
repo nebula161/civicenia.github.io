5800b11b5fbb58cdfa096bbd9678a114d679b1fc
Alexander <protonull@protonmail.com>
2022-12-14 16:04:53 +0000

    Add admin script

---
+++ _admin.ts
@@ -0,0 +1,10 @@
+Deno.run({
+    cmd: ["npx", "netlify-cms-proxy-server"],
+    stdin: "null", stdout: "inherit", stderr: "inherit"
+}).status();
+
+Deno.run({
+    cmd: ["bundle", "exec", "jekyll", "serve", "-w"],
+    env: { PORT: "8080" },
+    stdin: "null", stdout: "inherit", stderr: "inherit"
+}).status();
